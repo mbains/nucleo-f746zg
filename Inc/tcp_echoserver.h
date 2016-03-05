6b35f873d0d4c4932787ac7a083fd2039e214ae7
mbains <manjinder.bains@gmail.com>
2016-03-05 15:53:02 -0800

    add tcp echo server

---
+++ Inc/tcp_echoserver.h
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   tcp_echoserver.h
+ * Author: mbai
+ *
+ * Created on March 5, 2016, 1:50 PM
+ */
+
+#ifndef TCP_ECHOSERVER_H
+
+void tcp_echoserver_init(void);
+
+#define TCP_ECHOSERVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TCP_ECHOSERVER_H */
+
